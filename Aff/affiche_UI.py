50638ffe5fc03757e6921458bc3db344072313c8
Julien9969 <julien.roux404@gmail.com>
2023-07-03 18:19:54 +0200

    presque good

---
+++ Aff/affiche_UI.py
@@ -0,0 +1,154 @@
+from affiche_gen import *
+import ctypes as ct
+import tkinter as tk
+
+image: Image
+text: str
+text_input: tk.Entry
+imageList = [file for file in os.listdir("./") if file.endswith('.png') and not file.startswith('logo')]
+
+image_preview:tk.Label
+photo: ImageTk.PhotoImage
+settings: tuple[int, int, bool] = (50, 35, True)
+
+def preview_image():
+    global image
+    # Convert the image to PhotoImage
+    photo = ImageTk.PhotoImage(image.resize((640,360)))
+    try:
+        image_preview.destroy()
+    except Exception:
+        pass
+    # Create a label to display the image
+    image_preview = tk.Label(window, image=photo)
+    image_preview.image = photo
+    image_preview.grid(row=1, column=0, padx=5, pady=5)
+    image_preview.update()
+
+    # Run the tkinter event loop
+
+def switchSide(side_but: tk.Button):
+    global settings, image, text
+    if side_but.cget('text') == "Logo : Gauche":
+        side_but.config(bg="#35a4ff", text="Logo : Droite")
+        settings = (settings[0], settings[1], False)
+        image = renderImage(imageList[0], text, settings_=settings)
+    else:
+        side_but.config(bg="#246055", text="Logo : Gauche")
+        settings = (settings[0], settings[1], True)
+        image = renderImage(imageList[0], text, settings_=settings)
+
+    preview_image()
+
+def resizeLogo(logo_size_slider: tk.Scale):
+    global image, settings, text
+
+    settings = (logo_size_slider.get(), settings[1], settings[2])
+    image = renderImage(file=imageList[0], text=text, settings_=settings)
+    preview_image()
+
+def moveLogo(logo_position_slider: tk.Scale):
+    global image, text, settings
+
+    settings = (settings[0], logo_position_slider.get(), settings[2])
+    image = renderImage(imageList[0], text, settings_=settings)
+    preview_image()
+
+def setting_tab(window:tk):
+    global text_input
+    setting_tab = tk.Frame(window, width=200, height=200, bg="#202020")
+    setting_tab.grid(row=0, column=0, padx=5, pady=5, sticky="nsew")
+    setting_tab.grid_columnconfigure(1, weight=1)
+
+    # radiobutton and profile frame
+    buts_and_infos = tk.Frame(setting_tab, bg="#202020")
+    buts_and_infos.grid(row=0, column=1, padx=5, pady=5, sticky="ew")
+
+    side_but = tk.Button(setting_tab, text="Logo : Gauche", fg=('white') ,font=("Arial", 12, "bold"), bg="#246055")
+    side_but.grid(row=2, column=2, padx=5, pady=5, sticky="ew")
+    side_but.config(command=lambda: switchSide(side_but))
+
+    label_info = tk.Label(buts_and_infos, text="Mettre '/' dans le titre forcer le retour à la ligne où on le veut", fg=('white') ,font=("Arial", 12, "bold"), bg="#202020")
+    label_info.grid(row=0, column=1, padx=5, pady=5, sticky="ew")
+
+
+    slider_and_button_frame = tk.Frame(setting_tab, bg="#202020")
+    slider_and_button_frame.grid(row=2, column=1, padx=5, pady=5, sticky="ew")
+    slider_and_button_frame.grid_columnconfigure((0, 1), weight=1)
+    # create save button
+    save_button = tk.Button(slider_and_button_frame, text="Save", fg=('white') ,font=("Arial", 12, "bold"), bg="green", command= lambda : save_and_next(setting_tab))
+    save_button.grid(row=0, column=2, padx=5, pady=5)
+
+    # logo size slider
+    logo_size_slider = tk.Scale(slider_and_button_frame, from_=10, to=150, orient=tk.HORIZONTAL, bg="#202020", fg="white", label="Logo taille")
+    logo_size_slider.bind("<ButtonRelease-1>", lambda e : resizeLogo(logo_size_slider))
+    logo_size_slider.grid(row=0, column=0, padx=5, pady=5, sticky="ew")
+    logo_size_slider.set(50)
+
+    # logo position slider
+    logo_position_slider = tk.Scale(slider_and_button_frame, from_=0, to=100, orient=tk.HORIZONTAL, bg="#202020", fg="white", label="Logo distance bord")
+    logo_position_slider.bind("<ButtonRelease-1>", lambda e : moveLogo(logo_position_slider))
+    logo_position_slider.grid(row=0, column=1, padx=5, pady=5, sticky="ew")
+    logo_position_slider.set(35)
+
+    # text input
+    text_input = tk.Entry(setting_tab, bg="white", fg="black", font=("Arial", 12, "bold"), border=1)
+    text_input.grid(row=1, column=1, padx=10, pady=5, sticky="ew")
+    text_input.insert(0, text)
+
+    # text input button
+    text_input_button = tk.Button(setting_tab, text="Valider", fg=('white') ,font=("Arial", 12, "bold"), bg="#b27300", command=lambda: applyText(text_input.get()))
+    text_input_button.grid(row=1, column=2, padx=5, pady=5, sticky="ew")
+
+def applyText(text_:str):
+    global image, text
+    text = text_.upper()
+    image = renderImage(imageList[0], text, settings_=settings)
+    preview_image()
+
+def save_and_next(frame: tk.Frame):
+    global image, text, text_input
+    saveImage(image, text)
+    if len(imageList) - 1 > 0:
+        imageList.remove(imageList[0])
+        print(imageList[0])
+        text = imageList[0].upper().replace('.PNG', '')
+        text_input.insert(0, text)
+        image = renderImage(imageList[0], text, settings_=settings)
+        preview_image()
+    else:
+        image = Image.open('./assets/fini.png')
+        frame.destroy()
+        preview_image()
+
+
+def initWindow():
+    window = tk.Tk()
+    window.title("Preview")
+    window.resizable(False, False)
+    window.configure(bg="#202020")
+    # window.grid_columnconfigure(0, weight=1)
+    dark_title_bar(window)
+    return window
+
+def dark_title_bar(window):
+    """
+    MORE INFO:
+    https://learn.microsoft.com/en-us/windows/win32/api/dwmapi/ne-dwmapi-dwmwindowattribute
+    """
+    window.update()
+    set_window_attribute = ct.windll.dwmapi.DwmSetWindowAttribute
+    get_parent = ct.windll.user32.GetParent
+    hwnd = get_parent(window.winfo_id())
+    value = 2
+    value = ct.c_int(value)
+    set_window_attribute(hwnd, 20, ct.byref(value), 4)
+
+if __name__ == "__main__":
+    window = initWindow()
+    text = imageList[0].upper().replace('.PNG', '')
+    image = renderImage(file=imageList[0], text=text, settings_=settings)
+    setting_tab(window)
+    preview_image()
+    window.mainloop()
+    os.remove("./assets/image_temp.png")
--- Aff/affiche_gen.py
+++ Aff/affiche_gen.py
@@ -1,15 +1,17 @@
-from PIL import Image, ImageDraw, ImageFont, ImageFilter
+from PIL import Image, ImageDraw, ImageFont, ImageFilter, ImageTk
 import os
 
 FONT_SIZE = 62 
 FONT_COLOR = (255, 255, 255)  # white
 SHADOW_OFFSET = 5
 
+settings: tuple[int, int, bool]
+
 def openImage(file:str) -> Image:
     image = Image.open(file)
     image.resize((1280,720))
-    image.save("./image_temp.png", dpi=(72,72))
-    return Image.open("./image_temp.png")
+    image.save("./assets/image_temp.png", dpi=(72,72))
+    return Image.open("./assets/image_temp.png")
 
 def loadFont() -> ImageFont:
     return ImageFont.truetype("assets/Orbitron-Bold.ttf", FONT_SIZE)
@@ -18,36 +20,101 @@ def calculateTextPosition(image:Image, text:str, font:ImageFont) -> tuple:
     _,_ ,text_width, text_height = font.getbbox(text)
     x = (image.width - text_width) // 2
     y = (image.height - text_height) // 2
-    return (x,y)
+    
+    return (x, 280 +y)
 
-def addShadow(image:Image, text:str, font:ImageFont, x:int, y:int) -> Image:
+
+def addShadow(image:Image, text:str, font:ImageFont) -> Image:
     shadow_image = Image.new("RGBA", image.size, (0, 0, 0, 0))
     shadow_draw = ImageDraw.Draw(shadow_image)
+    _, _, text_width, text_height = font.getbbox(text)
+    if text_width > 1180:
+        top, bot = clac_bot_and_top(text)
+
+        x, y = calculateTextPosition(image, top, font)
+
+        shadow_position = (x + SHADOW_OFFSET, y - text_height + SHADOW_OFFSET)
+        shadow_draw.text(shadow_position, top, font=font, fill=(0, 0, 0, 128))
+    else:
+        bot = text
+        
+    x, y = calculateTextPosition(image, bot, font)
     shadow_position = (x + SHADOW_OFFSET, y + SHADOW_OFFSET)
-    shadow_draw.text(shadow_position, text, font=font, fill=(0, 0, 0, 128))
+    shadow_draw.text(shadow_position, bot, font=font, fill=(0, 0, 0, 128))
+
     return Image.alpha_composite(image.convert("RGBA"), shadow_image)
 
-def drawText(image:Image, text:str, font:ImageFont, x:int, y:int) -> Image:
+def clac_bot_and_top(text:str) -> tuple:
+    bot = ""
+    top = ""
+    words = text.split(" ")
+    for i in range(0, len(words)):
+        print(" ".join(words[i:]))
+        print(round(len(text) * 0.4) < len(bot))
+        if len(text) * 0.55 < len(top) and (len(" ".join(words[i:]))) < 1080:
+            bot += words[i] + " "
+        else:
+            top += words[i] + " "
+    return (top, bot)  
+
+def drawText(image:Image, text:str, font:ImageFont) -> Image:
     draw = ImageDraw.Draw(image)
-    draw.text((x, y), text, font=font, fill=FONT_COLOR, spacing=21)
+    _, _, text_width, text_height = font.getbbox(text)
+    if text_width > 1180:
+        top, bot = clac_bot_and_top(text)
+
+        x, y = calculateTextPosition(image, top, font)
+        draw.text((x, y - text_height), top, font=font, fill=FONT_COLOR, spacing=21)
+    else:
+        bot = text
+
+    x, y = calculateTextPosition(image, bot, font)
+    draw.text((x, y), bot, font=font, fill=FONT_COLOR, spacing=21)
+    
     return image
 
-def saveImage(image:Image, output_path:str):
-    image.save(output_path)
-    os.remove("./image_temp.png")
+def addLogoF(image:Image) -> Image:
+    logo = Image.open("./assets/logo.png")
+    # logo = logo.resize((int(logo.width * 0.5), int(logo.height * 0.5)))
+    
+    if settings[2] == True:
+        image.paste(logo, (1150,0))
+    else:
+        image.paste(logo, (35, 0))
+    return image
 
-if __name__ == "__main__":
-    file = "./white_image.png"
+def addLogoS(image:Image) -> Image:
+    logo = Image.open("./logo.png")
+    width, height = logo.size[0] * (settings[0] / 100), logo.size[1] * (settings[0] / 100)
+    print('w : ' + str(settings[0] / 100), "h : " + str(logo.size[0]))
+    logo = logo.resize((int(width), int(height)))
+
+    if settings[2] == True:
+        image.paste(logo, (settings[1], settings[1]), mask=logo)
+    else:
+        image.paste(logo, (image.width - logo.size[0] - settings[1], settings[1]), mask=logo)
+    return image
+
+def renderImage(file:str, text, settings_: tuple[int, int, bool] = (50, 35, True)) -> Image:
+    global settings 
+    settings = settings_
     image = openImage(file)
 
     font = loadFont()
     print(image.info.get('dpi'))
 
-    text = "FILM 1 - EIKICHI ONIZUKA"
-    x_pos, y_pos = calculateTextPosition(image, text, font)
+    text = text # 34 characters
 
-    image = addShadow(image, text, font, x_pos, y_pos)
+    image = addShadow(image, text, font)
+    image = drawText(image, text, font)
+    image = addLogoF(image)
+    image = addLogoS(image)
+    return image
 
-    image = drawText(image, text, font, x_pos, y_pos)
+def saveImage(image:Image, fileName:str):
+    os.mkdir("./png") if not os.path.exists("./png") else None
+    os.mkdir("./webp") if not os.path.exists("./webp") else None
 
-    saveImage(image, "./image_modifier.png")
+    image.save("./png/" + fileName + ".png", dpi=(72,72))
+    image.save("./webp/" + fileName + ".webp", dpi=(72,72))
+
